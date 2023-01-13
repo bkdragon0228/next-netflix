e1f30a51183aa4686c68bea847853f750fd60728
bkdragon0228 <rlaqjarb2067@naver.com>
2023-01-14 02:12:09 +0900

    feat : row 컴포넌트 slide 기능, skeleton 컴포넌트 생성 후 적용

--- components/row.js
+++ components/row.js
@@ -3,13 +3,18 @@ import React, { useState, useEffect } from "react";
 import axios from "../pages/api/axios";
 
 const RowContainer = styled.div`
+  position: relative;
+  background-color: black;
+`;
+
+const SlideContainer = styled.div`
   width: 100%;
   display: flex;
   overflow-x: scroll;
   overflow-y: hidden;
   padding: 20px 0 20px 20px;
   scroll-behavior: smooth;
-  background-color: black;
+  /* transition: all 0.2s ease-in-out; */
 
   &::-webkit-scrollbar {
     display: none;
@@ -30,6 +35,39 @@ const MoviePoster = styled.div`
   }
 `;
 
+const MovieSkelton = styled(MoviePoster)`
+  box-sizing: border-box;
+  position: relative;
+  background-image: none;
+  background-color: lightgray;
+  opacity: 0.3;
+  margin: 0 16px;
+
+  @keyframes loading {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(300px);
+    }
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 30px;
+    height: 100%;
+    background: linear-gradient(to right, #f2f2f2, #ddd, #f2f2f2);
+    animation: loading 1.5s infinite linear;
+  }
+
+  &:hover {
+    transform: none;
+  }
+`;
+
 const MovieTitle = styled.h2`
   padding-top: 16px;
   padding-left: 20px;
@@ -37,8 +75,49 @@ const MovieTitle = styled.h2`
   color: white;
 `;
 
+const RightArrow = styled.div`
+  width: 50px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 16px;
+  padding: 20px 16px;
+  background-color: black;
+  opacity: 0.3;
+  color: white;
+  font-size: 32px;
+  transition: all 0.3s ease;
+  z-index: 10;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const LeftArrow = styled.div`
+  width: 50px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 16px;
+  padding: 20px 16px;
+  background-color: black;
+  opacity: 0.3;
+  color: white;
+  font-size: 32px;
+  transition: all 0.3s ease;
+  z-index: 10;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
 export default function Row({ title, id, fetchUrl, isLarge }) {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchMovies();
@@ -46,28 +125,53 @@ export default function Row({ title, id, fetchUrl, isLarge }) {
 
   const fetchMovies = async () => {
     try {
+      await new Promise((r) => setTimeout(r, 3000)); // 3초 지연
       const res = await axios.get(fetchUrl);
       setMovies(res.data.results);
+      setIsLoading(false);
     } catch (err) {
       throw err;
     }
   };
+
   return (
-    <>
+    <RowContainer>
       <MovieTitle>{title}</MovieTitle>
-      <RowContainer>
-        {movies.map((movie) => (
-          <>
-            <MoviePoster
-              key={movie.id}
-              background={`https://image.tmdb.org/t/p/original/${
-                isLarge ? movie.poster_path : movie.backdrop_path
-              }`}
-              isLarge={isLarge === true ? true : false}
-            />
-          </>
-        ))}
-      </RowContainer>
-    </>
+      {/* innerwidth - 80 만큼 이동 */}
+      <LeftArrow
+        onClick={() =>
+          (document.getElementById(id).scrollLeft -= window.innerWidth - 80)
+        }
+      >
+        {"<"}
+      </LeftArrow>
+      <SlideContainer id={id}>
+        {isLoading
+          ? new Array(10)
+              .fill(1)
+              .map((_, i) => (
+                <MovieSkelton
+                  key={i}
+                  isLarge={isLarge === true ? true : false}
+                />
+              ))
+          : movies.map((movie) => (
+              <MoviePoster
+                key={movie.id}
+                background={`https://image.tmdb.org/t/p/original/${
+                  isLarge ? movie.poster_path : movie.backdrop_path
+                }`}
+                isLarge={isLarge === true ? true : false}
+              />
+            ))}
+      </SlideContainer>
+      <RightArrow
+        onClick={() =>
+          (document.getElementById(id).scrollLeft += window.innerWidth - 80)
+        }
+      >
+        {">"}
+      </RightArrow>
+    </RowContainer>
   );
 }
